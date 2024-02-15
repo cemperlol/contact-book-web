ac2544af3c703e5db69726b4ee578d4622ec38ec
cemperlol <baxeon96@yandex.ru>
2024-02-15 23:57:43 +0300

    init

---
+++ src/main/java/com/practice/contactbookweb/ContactBookWebApplication.java
@@ -0,0 +1,13 @@
+package com.practice.contactbookweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookWebApplication.class, args);
+	}
+
+}
