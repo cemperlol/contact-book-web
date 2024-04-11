5a1feca5b549ff8a02d333a6c2378cad09005ec3
cemperlol <baxeon96@yandex.ru>
2024-04-11 09:21:41 +0300

    partially completed edit menu

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,5 +1,6 @@
 const openAdditionButtons = document.querySelectorAll('[data-addition-target]')
 const closeAdditionButtons = document.querySelectorAll('[data-close-button]')
+const editButtons = document.querySelectorAll('[data-edit-target]')
 
 openAdditionButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -26,6 +27,16 @@ closeAdditionButtons.forEach(button => {
   })
 })
 
+
+editButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    var row = button.closest('tr')
+  
+    activateEditMode(row)
+  })
+})
+
+
 function openAdditionTab(additionTab) {
   if (additionTab == null) return
   additionTab.classList.add('active')
@@ -35,3 +46,27 @@ function closeAdditionTab(additionTab) {
   if (additionTab == null) return
   additionTab.classList.remove('active')
 }
+
+function activateEditMode(row) {
+  var inputs = row.querySelectorAll('input')
+  var paragraphs = row.querySelectorAll('p')
+  
+  inputs.forEach(input => {
+    input.classList.remove('invisible')
+  })
+  paragraphs.forEach(paragraph => {
+    paragraph.classList.add('invisible')
+  })
+}
+
+  //   if (inputs[0].classList.contains('invisible')) {
+
+  //   } else {
+  //     inputs.forEach(input => {
+  //       input.classList.add('invisible')
+  //     })
+  //     paragraphs.forEach(paragraph => {
+  //       paragraph.classList.remove('invisible')
+  //     })
+  //   }
+  // }
