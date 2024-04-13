43effc67d1d0ed6fd77f2dc5bcc192911efcb2a3
Алексей Романов <baxeon96@yandex.ru>
2024-04-13 23:02:45 +0300

    implemented index.html and affilated scripts and styling

--- src/main/resources/templates/js/script.js
+++ src/main/resources/templates/js/script.js
@@ -1,6 +1,7 @@
 const openAdditionButtons = document.querySelectorAll('[data-addition-target]')
 const closeAdditionButtons = document.querySelectorAll('[data-close-button]')
 const editButtons = document.querySelectorAll('[data-edit-target]')
+const cancelButtons = document.querySelectorAll('[data-edit-cancel]')
 
 openAdditionButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -18,24 +19,14 @@ closeAdditionButtons.forEach(button => {
     const actionButtonCells = document.getElementsByClassName('action-buttons-cell')
     let lastActionButtonCellId = actionButtonCells.length - 1
     actionButtonCells[lastActionButtonCellId].classList.remove('active')
-    const addittionButton = document.querySelectorAll('[data-addition-target]')
-    addittionButton.forEach(additionButton => {
+    const additionButton = document.querySelectorAll('[data-addition-target]')
+    additionButton.forEach(additionButton => {
       additionButton.classList.add('active')
     })
     const additionTab = button.closest('.addition-tab')
     closeAdditionTab(additionTab)
   })
 })
-
-
-editButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    var row = button.closest('tr')
-  
-    activateEditMode(row)
-  })
-})
-
 
 function openAdditionTab(additionTab) {
   if (additionTab == null) return
@@ -47,26 +38,81 @@ function closeAdditionTab(additionTab) {
   additionTab.classList.remove('active')
 }
 
-function activateEditMode(row) {
-  var inputs = row.querySelectorAll('input')
-  var paragraphs = row.querySelectorAll('p')
+editButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    var row = button.closest('tr')
   
+    activateEditMode(row)
+  })
+})
+
+function activateEditMode(row) {
+  var $row = $(row);
+  var columns = row.querySelectorAll('.editable-info')
+  var inputs = row.querySelectorAll('input')
+  var paragraphs = row.querySelectorAll('.contact-info')
+  var editButton = row.querySelector('.edit-button')
+  var removeButton = row.querySelector('.remove-button')
+  var editActionButtons = row .querySelectorAll('.edit-action-button')
+
+  if ($row.is(':nth-child(2n + 1)')) {
+    columns.forEach(column => {
+      column.style.backgroundColor = "#a6a6aee1"
+    })
+  } else {
+    columns.forEach(column => {
+      column.style.backgroundColor = "#a6a6aea2"
+    })
+  }
   inputs.forEach(input => {
     input.classList.remove('invisible')
   })
   paragraphs.forEach(paragraph => {
     paragraph.classList.add('invisible')
   })
+  editButton.classList.add('invisible')
+  removeButton.classList.add('invisible')
+  editActionButtons.forEach(editActionButton => {
+    editActionButton.classList.remove('invisible')
+  })
 }
 
-  //   if (inputs[0].classList.contains('invisible')) {
 
-  //   } else {
-  //     inputs.forEach(input => {
-  //       input.classList.add('invisible')
-  //     })
-  //     paragraphs.forEach(paragraph => {
-  //       paragraph.classList.remove('invisible')
-  //     })
-  //   }
-  // }
+cancelButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    var row = button.closest('tr')
+
+    cancelEditing(row)
+  })
+})
+
+function cancelEditing(row) {
+  var $row = $(row)
+  var columns = row.querySelectorAll('td')
+  var inputs = row.querySelectorAll('input')
+  var paragraphs = row.querySelectorAll('.contact-info')
+  var editButton = row.querySelector('.edit-button')
+  var removeButton = row.querySelector('.remove-button')
+  var editActionButtons = row .querySelectorAll('.edit-action-button')
+
+  if ($row.is(':nth-child(2n + 1)')) {
+    columns.forEach(column => {
+      column.style.backgroundColor = "#e1e8ec51"
+    })
+  } else {
+    columns.forEach(column => {
+      column.style.backgroundColor = "white"
+    })
+  }
+  inputs.forEach(input => {
+    input.classList.add('invisible')
+  })
+  paragraphs.forEach(paragraph => {
+    paragraph.classList.remove('invisible')
+  })
+  editButton.classList.remove('invisible')
+  removeButton.classList.remove('invisible')
+  editActionButtons.forEach(editActionButton => {
+    editActionButton.classList.add('invisible')
+  })
+}
